4133850689c6968a326c1539aa1e19177ebfe273
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2024-01-21 15:07:47 +0200

    Update CategoriesResource.php

--- app/Http/Resources/CategoriesResource.php
+++ app/Http/Resources/CategoriesResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,6 +19,7 @@ class CategoriesResource extends JsonResource
             'id'=>$this->id,
             'name'=>$this->name,
             'img'=>$this->img,
+            'products'=>count(Products::all()->where('cat_id',$this->id))
         ];
     }
 }
